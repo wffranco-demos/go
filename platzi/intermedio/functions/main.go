a02bcfd61c97a16c5d37f9fc229609b5e4106d6a
Willem Franco <willemffrancoc@gmail.com>
2024-08-10 05:03:01 +0000

    ejemplo funciones variadicas

--- platzi/intermedio/functions/main.go
+++ platzi/intermedio/functions/main.go
@@ -5,8 +5,22 @@ import (
 	"time"
 )
 
-// Testing anonymous functions
+func sum(items ...int) int {
+	total := 0
+	for _, item := range items {
+		total += item
+	}
+	return total
+}
+
+func getValues(x int) (double int, triple int) {
+	double = x * 2
+	triple = x * 3
+	return
+}
+
 func main() {
+	// Testing anonymous functions
 	c := make(chan int)
 	go func(c chan int) {
 		time.Sleep(2 * time.Second)
@@ -15,4 +29,11 @@ func main() {
 	fmt.Println("Waiting for goroutine")
 	<-c
 	fmt.Println("Finished")
+
+	// Testing variadic functions
+	fmt.Println(sum(1, 2, 3, 4, 5))
+
+	// Testing multiple return values
+	double, triple := getValues(2)
+	fmt.Println(double, triple)
 }
