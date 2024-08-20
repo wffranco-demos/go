be2feee688538fd35f0a95958f0b0cb86b05cd3d
Willem Franco <willemffrancoc@gmail.com>
2024-08-20 14:09:23 +0000

    Agregando bloqueo de solo lectura.

--- platzi/avanzado/sync/main.go
+++ platzi/avanzado/sync/main.go
@@ -8,28 +8,33 @@ import (
 
 var balance int = 100
 
-func Deposit(value int, wg *sync.WaitGroup, lock *sync.Mutex) {
+func Deposit(value int, wg *sync.WaitGroup, lock *sync.RWMutex) {
 	defer wg.Done()
 	lock.Lock()
 	// balance += value
 	tmp := balance
 	tmp += value
-	time.Sleep(1 * time.Second)
+	if value > 500 {
+		time.Sleep(1 * time.Second)
+	}
 	balance = tmp
 	lock.Unlock()
 }
 
-func Balance() int {
-	return balance
+func Balance(lock *sync.RWMutex) int {
+	lock.RLock()
+	b := balance
+	lock.RUnlock()
+	return b
 }
 
 func main() {
 	var wg sync.WaitGroup
-	var lock sync.Mutex
+	var lock sync.RWMutex
 	for i := 0; i < 5; i++ {
 		wg.Add(1)
 		go Deposit(i*100, &wg, &lock)
 	}
 	wg.Wait()
-	fmt.Println(Balance())
+	fmt.Println(Balance(&lock))
 }
